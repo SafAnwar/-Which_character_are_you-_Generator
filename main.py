c5c5d4ba4d810bf73d6f76e984e86119d510cdf3
SafAnwar <140514859+SafAnwar@users.noreply.github.com>
2025-01-16 19:35:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Day 5
+print("Marvel Movie Character Creator")
+print("--")
+spiderMan = input("Do you like 'hanging around'?: ")
+if spiderMan == "Yes":
+  print("Aha! You're Spider-man! Hi!")
+else:
+  print("Then you're not Spider-man")
+  korg = input("Do you have a 'gravelly' voice?: ")
+  if korg == "Yes":
+    print("Aha! You're Korg! Hi!")
+  else:
+    print("Aww, then you're not Korg")
+    captainMarvel = input("Do you often feel 'Marvelous'?: ")
+    if captainMarvel == "Yes":
+      print("Aha! You're Captain Marvel! Hi!")
+    else:
+      print("I actually don't know")
